564795d0bdae7e910b558dd04be45fd6c4801f1d
teke4869 <r08028099448@icloud.com>
2023-09-25 17:12:30 +0900

    ✨ 病院データの作成

---
+++ front/src/data.ts
@@ -0,0 +1,22 @@
+export const servicesData = [
+    {
+      title: '一般診療',
+      description: '一般的な健康問題の診療',
+      image: '/images/general_practice.jpg',
+    },
+    {
+      title: '歯科治療',
+      description: '歯科検診と治療',
+      image: '/images/dental.jpg',
+    },
+    {
+      title: '眼科診療',
+      description: '視力検査と眼の健康管理',
+      image: '/images/ophthalmology.jpg',
+    },
+    {
+      title: '内科診療',
+      description: '内部の健康問題の診療',
+      image: '/images/internal_medicine.jpg',
+    },
+  ];

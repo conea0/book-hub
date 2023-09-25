124fcf0ed3af9640a1039b364e788d743d0c080b
teke4869 <r08028099448@icloud.com>
2023-09-25 18:04:35 +0900

    ✨ カレンダーページの作成

---
+++ front/src/app/calendar/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Calendar from './calendar';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>週間カレンダー</h1>
+      <Calendar />
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
+
+

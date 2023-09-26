caa4765113d8602e7115d85740d79d4492b3c2ba
teke4869 <r08028099448@icloud.com>
2023-09-26 22:23:56 +0900

    ✨ サンプルデータの作成

--- front/src/data.ts
+++ front/src/data.ts
@@ -1,57 +1,34 @@
-export const servicesData = [
-    {
-      title: '一般診療',
-      description: '一般的な健康問題の診療',
-      image: '/images/general_practice.jpg',
-    },
-    {
-      title: '歯科治療',
-      description: '歯科検診と治療',
-      image: '/images/dental.jpg',
-    },
-    {
-      title: '眼科診療',
-      description: '視力検査と眼の健康管理',
-      image: '/images/ophthalmology.jpg',
-    },
-    {
-      title: '内科診療',
-      description: '内部の健康問題の診療',
-      image: '/images/internal_medicine.jpg',
-    },
-  ];
-
   export const hospitalData = [
     {
       id: 1,
-      title: '病院01',
+      name: '病院01',
+      address: '東京都新宿区',
       image: '/images/hospital01.jpg',
       description: '病院01の説明',
-      address: '東京都新宿区',
       phone: '03-1234-5678',
     },
     {
       id: 2,
-      title: '病院02',
+      name: '病院02',
+      address: '東京都渋谷区',
       image: '/images/hospital02.jpg',
       description: '病院02の説明',
-      address: '東京都渋谷区',
       phone: '03-1234-5678',
     },
     {
       id: 3,
-      title: '病院03',
+      name: '病院03',
+      address: '東京都豊島区',
       image: '/images/hospital03.jpg',
       description: '病院03の説明',
-      address: '東京都豊島区',
       phone: '03-1234-5678',
     },
     {
       id: 4,
-      title: '病院04',
+      name: '病院04',
+      address: '東京都千代田区',
       image: '/images/hospital04.jpg',
       description: '病院04の説明',
-      address: '東京都千代田区',
       phone: '03-1234-5678',
     }
  ];

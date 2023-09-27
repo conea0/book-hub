c986f4e264f690e2d5295a643d76c2cfefc0c19a
teke4869 <r08028099448@icloud.com>
2023-09-27 22:56:42 +0900

    ✨ ヘッダーの追加

--- front/src/app/hospital/[id]/page.tsx
+++ front/src/app/hospital/[id]/page.tsx
@@ -1,23 +1,27 @@
 'use client';
-'use client';
 // pages/hospital/[id].tsx
-import { Layout } from 'antd';
+import { Layout,Menu} from 'antd';
 import  HospitalDetail  from './HospitalDetail';
-import { useSearchParams } from 'next/navigation';
 import { hospitalData } from '@/data';
 
-const { Content } = Layout;
+const { Header, Content, Footer } = Layout;
 
 const HospitalDetailPage = ({ params }: { params: { id: string } }) => {
   const id = Number(params.id) - 1;
   return (
-    <Layout>
+    <Layout style={{minHeight:100}}>
+       <Header>
+        <div/>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">病院予約サイト</Menu.Item>
+        </Menu>
+      </Header>
       <Content>
-        <div className="site-layout-content">
-          <h1>病院詳細ページ</h1>
+        <div>
           <HospitalDetail hospitalData={hospitalData[id]}  />
         </div>
       </Content>
+      <Footer style={{ textAlign: 'center' }}>予約アプリ ©2023 Created by Your Company</Footer>
     </Layout>
   );
 };

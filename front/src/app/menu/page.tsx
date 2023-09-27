b6c540d958904e863a56219c5335488a4e4df1c0
teke4869 <r08028099448@icloud.com>
2023-09-27 22:59:53 +0900

    ✨ 検索機能の追加とkindに基づいてカードを表示させた

--- front/src/app/menu/page.tsx
+++ front/src/app/menu/page.tsx
@@ -1,50 +1,82 @@
 'use client';
-import React from 'react';
-import { Layout, Menu, Breadcrumb, Card,Row, Col } from 'antd';
+import React, { useState } from 'react';
+import { Layout, Menu, Breadcrumb, Card, Row, Col, Input } from 'antd';
 import './menu.css';
 import { hospitalData } from '@/data';
 import DetailButton from '../hospital/DetailButton';
-
+import {hospital} from '@/types'
 
 const { Header, Content, Footer } = Layout;
 
+
 const HospitalDetailPage = () => {
+  const [searchValue, setSearchValue] = useState<string>(''); // 検索バーの値を管理するステート
+
+  // 検索バーの値が変更されたときのハンドラ
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
+
+  // フィルタリングされた病院データ
+  const filteredHospitals = hospitalData.filter((item: hospital) => {
+    const lowercaseSearchValue = searchValue.toLowerCase();
+    return (
+      item.name.toLowerCase().includes(lowercaseSearchValue) ||
+      item.kind.toLowerCase().includes(lowercaseSearchValue)
+    );
+  });
+
+  // 種別ごとに病院データをグループ化
+  const groupedHospitals: { [kind: string]: hospital[] } = {};
+  filteredHospitals.forEach((hospital) => {
+    if (!groupedHospitals[hospital.kind]) {
+      groupedHospitals[hospital.kind] = [];
+    }
+    groupedHospitals[hospital.kind].push(hospital);
+  });
+
   return (
     <Layout className="layout">
       <Header>
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">ホーム</Menu.Item>
-          <Menu.Item key="2">予約</Menu.Item>
-          <Menu.Item key="3">診療所</Menu.Item>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">病院予約サイト</Menu.Item>
         </Menu>
+        <Input
+          placeholder="病院名または種別を検索"
+          style={{ width: 300, float: 'right', marginTop: 15 }}
+          value={searchValue}
+          onChange={handleSearchChange}
+        />
       </Header>
       <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>ホーム</Breadcrumb.Item>
-          <Breadcrumb.Item>予約</Breadcrumb.Item>
+        <Breadcrumb style={{ margin: '30px 0' }}>
         </Breadcrumb>
         <div className="site-layout-content">
-          <h1>予約</h1>
-          <Row gutter={[16, 16]}>
-            {hospitalData.map((item, index) => (
-              <Col span={8} key={index}>
-                <Card
-                  className="service-card"
-                  cover={<img alt={item.name} src={item.image} />}
-                >
-                  <h3>{item.name}</h3>
-                  <p>{item.description}</p>
-                  <DetailButton hospitalId={String(item.id)} />
-                </Card>
-              </Col>
-            ))}
-          </Row>
+          {Object.keys(groupedHospitals).map((kind, index) => (
+            <div key={index}>
+              <h1 className='h1'>{kind}</h1>
+              <div style={{display:"flex",overflowX:"scroll"}}>
+                {groupedHospitals[kind].map((item, index) => (
+                  <Col span={8} key={index} style={{ marginBottom: '20px' }}>
+                    <Card
+                      className="service-card"
+                      cover={<img src={item.image} alt={item.name}/>}
+                    >
+                      <h3>{item.name}</h3>
+                      <p>{item.address}</p>
+                      <DetailButton hospitalId={String(item.id)} />
+                    </Card>
+                  </Col>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>予約アプリ ©2023 Created by Your Company</Footer>
     </Layout>
   );
-}
+};
 
 export default HospitalDetailPage;
